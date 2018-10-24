e97ebabe02bf79558255cf887fa08356c0fa6e42
Claim <shenling.ywt@alibaba-inc.com>
2018-10-24 14:19:20 +0800

    work fine

--- readList.js
+++ readList.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 module.exports = function (filePath, encoding) {
-  return new Promise(resolve => {
+  return new Promise((resolve, reject) => {
     fs.readFile(filePath, {encoding: encoding || 'utf8'}, (err, data) => {
-      if (err) return;
+      if (err) reject(err);
       const links = data.replace('\r\n', '\n').split('\n').filter(link => link);
       resolve(links);
     });
